4f0ba2d97efd1747b83cb444d56f6921362ca0ec
Alex Martin <amartin@paybyphone.com>
2013-10-10 22:19:50 -0700

    basic MVC site to allow upload of an image

---
+++ Ocr_Server/Ocr_Server/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ocr_Server
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Ocr_Server/Ocr_Server/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ocr_Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new HttpStatusCodeResult(200);
+        }
+
+        [HttpPost]
+        public ActionResult uploadImage(HttpPostedFileBase file, int userId)
+        {
+            if (file != null && file.ContentLength > 0) 
+                return new HttpStatusCodeResult(200);
+
+            return new HttpStatusCodeResult(400);
+        }
+    }
+}
